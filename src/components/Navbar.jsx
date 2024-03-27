5238c98581c85f2b3efe7977f962c9a9a12c3123
Suraj Kumar <kumarsuraj345678@gmail.com>
2024-03-27 12:43:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className=" bg-[#001e2b] text-white ">
+      {/* Navbar container */}
       <div className="mycontainer flex items-center px-4 py-5 justify-between">
+        {/* Logo */}
         <div className="logo font-bold text-2xl flex flex-row items-center">
           <span className="text-[#00ed64]">S3CURE</span>
           <span className="">PASS </span>
@@ -13,6 +15,7 @@ const Navbar = () => {
             className="w-7 mx-1"
           />
         </div>
+        {/* Navigation links (commented out) */}
         {/* <ul>
           <li className="flex gap-4">
             <a className="hover:font-bold" href="/">
