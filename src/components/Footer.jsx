31a5df4e0ac466e818c3fec8cb886a2da518f5cc
Suraj Kumar <kumarsuraj345678@gmail.com>
2024-03-27 12:54:26 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-[#001e2b] text-white flex flex-col justify-center items-center w-full">
+      {/* Logo Section */}
       <div className="logo font-bold text-2xl flex flex-row items-center">
         <span className="text-[#00ed64]">S3CURE</span>
         <span className="">PASS </span>
         <img src="/secureIcon.png" alt="secureIcon" className="w-7 mx-1" />
       </div>
+
+      {/* Attribution Section */}
       <div className="flex">
         Made with <span className="text-red-500 px-1">&hearts;</span>
         by
